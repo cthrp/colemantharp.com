8763228791d9f0a3adf69889569f44bfa3156e1a
Coleman Tharp <colemantharp@me.com>
2017-05-19 06:40:31 -0500

    fewewefwfewfew

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
   var pixels = $(document).scrollTop()
 
-  var font = pixels  / 32 + 10
+  var font = pixels + 32 + 10
   var email = "hi@colemantharp.design"
 
   $("div").css("font-size", font)
